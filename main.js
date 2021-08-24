d20edf9c2588f73d0cd3d454c9b493fa0e32dd67
Arnavkanugo <72987461+Arnavkanugo@users.noreply.github.com>
2021-08-24 15:32:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+img="";
+status="";
+function draw()
+{
+    image(img,0,0,600,400);
+fill("#9950C7");
+text("dog",45,75);
+noFill();
+stroke("#C75097");
+rect(30,60,450,350);
+fill ("#abdbe3");
+text("cat",777,777);
+stroke("#3ea9cb");
+rect(444,91,270,320);
+}
+function preload()
+{
+       img=loadImage("dog_cat.jpg");
+}
+function setup()
+{
+    canvas=createCanvas(600,400);
+    canvas.center();
+objectDetector=ml5.objectDetector("cocossd",modelloaded);
+document.getElementById("staus").innerHTML="status detecting object";
+}
+function modelloaded(){
+console.log("modelloaded");
+status=true;
+objectDetector.detect(img,gotResult);
+
+}
+function gotResult(error,results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+
+else{
+ console.log(results);
+}
+}
